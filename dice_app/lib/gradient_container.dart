8737bd216e3b43ab1ff7560854c09df72fd9b2ca
Niladri <niladrisengit12@gmail.com>
2023-09-20 00:26:07 +0530

    day-03

--- dice_app/lib/gradient_container.dart
+++ dice_app/lib/gradient_container.dart
@@ -1,21 +1,36 @@
-import 'package:dice_app/text_widget.dart';
+import 'package:dice_app/dice_roller.dart';
 import 'package:flutter/material.dart';
-class ContainerClass extends StatelessWidget {
 
+const beginAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+
+
+class ContainerClass extends StatelessWidget {
   // ContainerClass({key}):super(key: key);
-  const ContainerClass({super.key});
+  //  const ContainerClass(this.startColor,this.endColor,{super.key});
+  // const ContainerClass(this.colors, {super.key});
+   const ContainerClass({required this.colors, super.key});
+
+  // const ContainerClass.gradientColor({ super.key}): color1 = Colors.black12;
+
+  //  final Color startColor;
+  //  final Color endColor;
+
+  final List<Color> colors;
+
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [
-        Color.fromARGB(255, 101, 58, 175),
-        Color.fromRGBO(7, 37, 119, 1)
-      ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: colors, begin: beginAlignment, end: endAlignment)),
       child: const Center(
-        child: TextWidget(),
+        // child: TextWidget(mainText),
+        child: DiceRoller(),
       ),
     );
   }
 }
-
--- dice_app/lib/text_widget.dart
+++ dice_app/lib/text_widget.dart
@@ -1,14 +1,20 @@
+
 import 'package:flutter/material.dart';
 
+
+
 class TextWidget extends StatelessWidget {
-  const TextWidget({super.key});
+   const TextWidget(this.mainText,{super.key});
+
+  final String mainText;
+  
   @override
   Widget build(BuildContext context) {
-    return const Text("Namaste Flutter",
-        style: TextStyle(
-            fontSize: 36,
+    return Text(mainText,
+        style: const TextStyle(
+            fontSize: 30,
             color: Colors.cyan,
             fontWeight: FontWeight.w800,
-            fontFamily: 'Roboto'));
+            fontFamily: 'Poppins'));
   }
 }
